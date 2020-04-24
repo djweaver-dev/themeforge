12ee2eef0e0fb64aaa1e039fd9902b38c80c0a15
djweaver-dev <djweaver@djweaver.dev>
2020-04-24 04:40:26 -0400

    navbar html added

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 'use strict'
 const electron = require('electron');
-const { app, BrowserWindow, ipcMain } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 function createWindow(){
     let win = new BrowserWindow({
         width: 900,
-        height: 450,
+        height: 600,
         webPreferences: {
             nodeIntegration: true
         }
     })
-    win.loadFile('../index.html')
+    win.loadFile('index.html')
+    //Menu.setApplicationMenu(null)
 }
 app.whenReady().then(createWindow)
