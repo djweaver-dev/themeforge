11f8a154b2bbdfa06d68bede5cc5a3fe8acc0aeb
djweaver-dev <djweaver@djweaver.dev>
2020-04-25 21:06:23 -0400

    added async function for setting theme on UI

--- js/renderer.js
+++ js/renderer.js
@@ -4,30 +4,40 @@ const { ipcRenderer } = electron;
 const fs = require('fs');
 const path = require('path');
 const menu = require('./js/menu.js');
+//const editor = require('./js/editor.js');
 
-//loads default dataset
-let data = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../themeforge/themes/default.json')))
-
-function renderColors(dataSet){
-    // let envElems = document.querySelectorAll('.env');
-    // for(let i = 0; i < envElems.length; i++){
-    //     //do something something
-    // }
-    document.getElementById('env-activity').style.backgroundColor =
-    dataSet.colors['activityBar.background'];
-    document.getElementById('env-sidebar').style.backgroundColor =
-    dataSet.colors['sideBar.background'];
-    document.getElementById('env-editor').style.backgroundColor =
-    dataSet.colors['editor.background'];
-    document.getElementById('env-panel').style.backgroundColor =
-    dataSet.colors['panel.background'];
-    document.getElementById('env-status').style.backgroundColor =
-    dataSet.colors['statusBar.background'];
+//loads dataset
+function importTheme(importPath){
+    let data;
+    if(importPath === 'default'){
+        data = JSON.parse
+        (fs.readFileSync(path.resolve(__dirname,
+            '../themeforge/themes/default.json')));
+    } else {
+        data = JSON.parse
+        (fs.readFileSync(path.resolve(__dirname,
+            importPath)));
+    }
+    return data;
+}
+async function setTheme(importPath){
+    let colorMap = [
+        { editor: 'env-activity', data: 'activityBar.background' },
+        { editor: 'env-sidebar', data: 'sideBar.background' },
+        { editor: 'env-editor', data: 'editor.background' },
+        { editor: 'env-panel', data: 'panel.background' },
+        { editor: 'env-status', data: 'statusBar.background' }
+    ]
+    let dataSet = await importTheme(importPath);
+    for(let i = 0; i < colorMap.length; i++){
+        document.getElementById
+        (colorMap[i].editor).style.backgroundColor =
+        dataSet.colors[colorMap[i].data];
+    }
 }
 
-renderColors(data)
-
-
+//sets our initial default theme
+setTheme('default')
 
 //this delegates a single click event within html 
 //element to control the entire program
@@ -35,55 +45,56 @@ document.onclick = function(event) {
     let targetId = event.target.id;
     let target = document.getElementById(targetId);
     let idArray = targetId.split('-');
+    console.log(idArray)
 
     switch(idArray[0]){
         case 'menu':
             //menu routing module
-            let command = menu.control(targetId, idArray, target);
+            let cmd = menu.control(targetId, idArray, target);
 
             //menu command execution
-            switch(command.action){
+            switch(cmd.action){
                 //import loads our theme JSON into data object
                 case 'import':
-                    data = JSON.parse(fs.readFileSync(path.resolve(__dirname, command.importPath)));
-                    renderColors(data);
-                    console.log(command.importPath + " loaded")
+                    // data = JSON.parse(fs.readFileSync(path.resolve(__dirname, cmd.importPath)));
+                    // renderColors(data);
+                    setTheme(cmd.importPath)
                     break;
                 //export handles Save and Save As
                 case 'export':
-                    fs.writeFile(command.exportPath, JSON.stringify(data), (error) => {
-                        console.log(command.exportPath);
-                        if(error) console.log('whoopsie!');
-                    });
+                    fs.writeFile(cmd.exportPath, JSON.stringify(data), (error) => {
+                        console.log(cmd.exportPath);
+                        if(error) console.log('whoopsie!')
+                    })
                     break;
                 //nav simulates clicks on hidden anchors
                 case 'nav':
-                    document.getElementById(command.target)
+                    document.getElementById(cmd.target)
                     .dispatchEvent(new MouseEvent('click'))
                     break;
-                //sends exit command to main
+                //sends exit cmd to main
                 case 'exit':
                     console.log('exit command received')
-                    ipcRenderer.send('exit');
+                    ipcRenderer.send('exit')
                     break;
-                //sends devtools command to main
+                //sends devtools cmd to main
                 case 'devtools':
-                    ipcRenderer.send('devtools');
+                    ipcRenderer.send('devtools')
                     break;
-                //sends refresh command to main
+                //sends refresh cmd to main
                 case 'reload':
-                    ipcRenderer.send('reload');
+                    ipcRenderer.send('reload')
                     break;
                 //any unaccounted for clicks log id 
                 //of element to console
                 default: 
-                    console.log(`${command.action} action from id="${targetId}"`)
+                    console.log(`${cmd.action} action from id="${targetId}"`)
                     break;
             }
             break;
         case 'edit':
             //modularized function to control editor
-            //editControl(idArray);
+            //let cmd = editor.control(targetId, idArray, target);
             break;
     }
 }
